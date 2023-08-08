cdc9bba63f9507d213d3f02fb47b3237eb1dcd59
Prajjawal Pandit <54879033+prajjawal1604@users.noreply.github.com>
2023-08-08 22:43:43 +0530

    Time: 2166 ms (32.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH cte AS
+(SELECT *, RANK() OVER(PARTITION BY product_id
+ORDER BY year) AS rnk
+FROM Sales)
+
+
+SELECT product_id, year as first_year, quantity, price
+FROM cte
+WHERE rnk = 1
