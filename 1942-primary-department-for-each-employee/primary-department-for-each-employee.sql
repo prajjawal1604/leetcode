35c6651edd8cdc0a635554e28561400f5ad0be21
Prajjawal Pandit <54879033+prajjawal1604@users.noreply.github.com>
2023-08-10 17:45:51 +0530

    Time: 1181 ms (61.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+WITH cte AS
+(SELECT employee_id, CASE WHEN COUNT(department_id) = 1
+THEN department_id
+WHEN COUNT(department_id) > 1 THEN SUM((primary_flag = 'Y')*department_id) END AS department_id
+FROM Employee
+GROUP BY employee_id)
+
+
+SELECT * FROM cte
+WHERE department_id != 0
