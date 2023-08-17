7ab8e41ba085b304a8bf521b7127d698520db21c
Prajjawal Pandit <54879033+prajjawal1604@users.noreply.github.com>
2023-08-17 21:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (55.92%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -4,9 +4,8 @@ public:
         sort(citation.begin(),citation.end());
         int n=citation.size();
 
-        for(int i=n-1,j=1;i>=0;--i,++j){
-            // cout<<citation[i]<<endl;
-            if(j>citation[i]) return j-1;}
+        for(int i=n-1,j=1;i>=0;--i,++j)
+            if(j>citation[i]) return j-1;
         
         return n;
     }
