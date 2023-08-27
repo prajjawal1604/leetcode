da1a16c4f715a5618f0b975569ebb89444a82539
Prajjawal Pandit <54879033+prajjawal1604@users.noreply.github.com>
2023-08-27 21:26:03 +0530

    Time: 41 ms (97.09%) | Memory: 232.2 MB (22.28%) - LeetSync

---
+++ 403-frog-jump/frog-jump.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool jump(vector<int>& stones, int i, int k, int des, vector<vector<int>> &dp){
+        if(i==stones.size()-1) return true;
+
+        while(stones[i]<des){
+            ++i;
+            if(i==stones.size()) return false;
+        }
+
+        if(stones[i]>des || k==0) return false;
+
+        if(dp[i][k] != -1) return dp[i][k];
+
+        // cout<<i<<" "<<k<<" "<<des<<endl;
+
+        return dp[i][k] = jump(stones,i,k-1,des+k-1,dp) || jump(stones,i,k,des+k,dp) || jump(stones,i,k+1,des+k+1,dp);
+        
+
+    }
+    bool canCross(vector<int>& stones) {
+        vector<vector<int>> dp(stones.size(),vector<int>(stones.size(),-1));
+        return jump(stones,0,1,stones[0]+1,dp);
+    }
+};
