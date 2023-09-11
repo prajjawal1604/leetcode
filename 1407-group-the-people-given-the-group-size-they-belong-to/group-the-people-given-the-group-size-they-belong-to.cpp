52ba321e922b629166e783c554e411e0225c264d
Prajjawal Pandit <54879033+prajjawal1604@users.noreply.github.com>
2023-09-11 16:11:51 +0530

    Time: 6 ms (90.23%) | Memory: 13.6 MB (28.92%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& grp) {
-         ios_base::sync_with_stdio(false);
+        ios_base::sync_with_stdio(false);
         unordered_map<int, queue<int>> umap;
         vector<vector<int>> ans;
         vector<int> temp;
@@ -16,11 +16,11 @@ public:
             while(i.second.size()){
                 ++k;
                 temp.push_back(i.second.front());
-                cout<<k<<" "<<i.second.front()<<endl;
+                // cout<<k<<" "<<i.second.front()<<endl;
                 i.second.pop();
                 if(k==i.first || !i.second.size()){
                     ans.push_back(temp);
-                    cout<<"Pushing"<<endl;
+                    // cout<<"Pushing"<<endl;
                     temp.clear();
                     k=0;
                 }
